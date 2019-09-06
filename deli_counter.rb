ec84b464e655736416e8e8d77820cf5ccfd3ef40
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-06 22:53:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+$customer_count = 0
 def line(katz_deli)
   temp = ""
   if katz_deli.empty?
@@ -15,6 +16,13 @@ def take_a_number(katz_deli,last_person)
   katz_deli.push(last_person)
   puts "Welcome, #{katz_deli.last}. You are number #{katz_deli.index(last_person) + 1} in line."
 end
+
+def take_a_number_2()
+  $customer_count+= 1
+  puts "You are currently #{$customer_count} in line."
+end
+take_a_number_2
+take_a_number_2
 
 def now_serving(katz_deli)
   if katz_deli.empty?
